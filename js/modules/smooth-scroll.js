c1d15abd4036dd0b5f83d64365a5d0ee2031d1b6
Anton <aleikinaa98@gmail.com>
2020-11-16 01:19:42 +0200

    Adaptation bugs fixed for all sections & some styles added.

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -21,6 +21,25 @@ export default function smoothScroll () {
 
     if (window.matchMedia('(max-width: 767px)').matches) {
 
+        const anchors = document.querySelectorAll('.section-link__link');
+
+        anchors.forEach((anchor) => {
+
+            anchor.addEventListener("click", (e) => {
+                e.preventDefault();
+    
+                const blockID = anchor.getAttribute('href').substr(1);
+                    
+                document.getElementById(blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        });
+    }
+
+    if (window.matchMedia('(max-width: 575px)').matches) {
+
         const anchor = document.querySelector('.section-link_mobile__link');
         
         anchor.addEventListener("click", (e) => {
